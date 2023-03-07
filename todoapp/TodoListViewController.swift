db832c2636e6b6a43410d673f353c09283241faa
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-03-07 13:50:53 -0800

    adds functionality to allow user to click on the todo item, and use the accessories property to check and uncheck item

--- todoapp/TodoListViewController.swift
+++ todoapp/TodoListViewController.swift
@@ -31,11 +31,27 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let todo = itemArray[indexPath.row]
        
+        // withIdentifier needs to be the same as the unique Identifier set in the table view cell attributes
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = todo
         
         return cell
     }
-
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // adds a checkmark on the todo item that was selected using the accessories in the attributes inspector
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // highlight the one selected in grey and then removes that highlight
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+   
+    }
 }
 
