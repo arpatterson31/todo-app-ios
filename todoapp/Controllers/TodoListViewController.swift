399237039ff3f7be48a3f2b19a3faaffb23a6f24
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-03-08 17:06:59 -0800

    implemented the read from my sqlite database and can now pull saved items

--- todoapp/Controllers/TodoListViewController.swift
+++ todoapp/Controllers/TodoListViewController.swift
@@ -26,7 +26,7 @@ class TodoListViewController: UITableViewController {
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
                
-//        loadItems()
+        loadItems()
     }
 
     
@@ -115,15 +115,13 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-//    func loadItems(){
-//        if let data = try? Data(contentsOf: dataFilePath!){
-//            let decoder = PropertyListDecoder()
-//            do {
-//                itemArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("Error encoding item array, \(error)")
-//            }
-//        }
-//    }
+    func loadItems(){
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
 }
 
