6bd34b814b3c92bcd187d706fb8382477dbd67d8
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-03-09 11:51:17 -0800

    adds a new controller - allowing users to see categories of items then moving to the item list view

---
+++ todoapp/Controllers/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryViewController.swift
+//  todoapp
+//
+//  Created by Audrey Patterson on 3/9/23.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
